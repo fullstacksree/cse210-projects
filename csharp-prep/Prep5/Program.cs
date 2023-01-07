db93c549453427f7e26b7e6f71ac4569a6e03603
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-07 20:24:27 +0530

    Finished Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int SquaredrootNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, SquaredrootNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the C# Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squareroot = number * number;
+        return squareroot;
+    }
+
+    static void DisplayResult(string name, int squareroot)
+    {
+        Console.WriteLine($"{name}, the squareroot of your number is {squareroot}");
     }
 }
