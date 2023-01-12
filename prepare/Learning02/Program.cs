14fa7f39df7bb6afed4d297df80395c0ed836109
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-12 22:16:33 +0530

    Finished Learn2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "HR Executive";
+        job1._company = "Patra";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new job();
+        job2._jobTitle = "Senior HR";
+        job2._company ="HSBC";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Steve Martin";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
     }
 }
