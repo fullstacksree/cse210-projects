138ce03375c6c8fae811b7f8041677cf999bcd99
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-15 10:19:13 +0530

    made change

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -3,13 +3,14 @@ class prompt{
         try{
             string[] lines = System.IO.File.ReadAllLines(_file);
             foreach (string line in lines){
-                _prompts.Add(line);
+                _prompt.Add(line);
             }
-        }catch (FileNotFoundException e){
-            using (StreamWriter outputFile = new StreamWriter(_file)){
+        }catch (FileNotFoundException){
+            using (StreamWriter outputFile = new StreamWriter(_file))
+            {
                 outputFile.WriteLine("What will be my first thing to do on this day?");
-                outputFile.WriteLine("Which scripture keeps me lifted while reading scriptures?")
-                outputFile.WriteLine("How can I seek the guidance of my Heavenly Father?")
+                outputFile.WriteLine("Which scripture keeps me lifted while reading?");
+                outputFile.WriteLine("How can I seek the guidance of my Heavenly Father?");
             }
         }
     }
