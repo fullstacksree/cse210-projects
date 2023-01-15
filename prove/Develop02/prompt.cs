a774a2d27fe41514fda282318c9d5f539b5c5655
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-15 12:17:49 +0530

    made changes

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -3,8 +3,6 @@ public  List<string> _prompt = new List<string>
     {
         "What will be my first thing to do on this day?",
         "What scripture keeps you lifted while reading?",
-        "How can I seek the guidance of my Heavenly Father?",
-        "When is the last book you read?",
-        "How time you study for this semester?"
+        "How can I seek the guidance of my Heavenly Father?"
     }
 }
