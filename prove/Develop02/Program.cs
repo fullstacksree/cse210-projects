8221aed54b8a597aaff0006d84b30ce924853e3b
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-15 09:13:19 +0530

    made changes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,33 +1,33 @@
 using System;
-using System.Collections.Generic;
-
 class Program
 {
     static void Main(string[] args)
     {
-        bool flag = true;
+        Console.WriteLine("Welcome to Daily Journal");
+
+        bool = true;
         do {
             Console.WriteLine("\n Select one of the below choices:");
-            Console.Write("1. Write 2. Display 3. Load 4. Save 5.Quit")
-            int choice = int.Parse(Console.ReadLince());
+            Console.Write("1. Write 2. Display 3. Load 4. Save 5.Quit");
+            int choice = int.Parse(Console.ReadLine());
             Journal myJournal = new Journal();
             Prompt myPrompts = new Prompt();
             switch(choice){
-                entry 1:
+                case 1:
                     myJournal.Write();
                     break;
-                entry 2:
+                case 2:
                     myJournal.Display();
                     break;
-                entry 3:
+                case 3:
                     myJournal.Load();
                     break;
-                entry 4:
+                case 4:
                     myJournal.Save();
                     break;
-                entry 5:
+                case 5:
                     myJournal.Quit()
             }
-        }while(flag);
+        }
     }
 }
