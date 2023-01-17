dad000d24611743fbec9f20ed8545d3e090416fc
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-17 07:59:16 +0530

    made saves

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -1,8 +1,15 @@
-public class Prompt{
-public  List<string> _prompt = new List<string>
+public class Prompt
+{
+    List<string> _prompt;// = new List<string>();
+
+    public Prompt()
     {
+        _prompt = new List<string> 
+        {
         "What will be my first thing to do on this day?",
         "What scripture keeps you lifted while reading?",
         "How can I seek the guidance of my Heavenly Father?"
+        };
     }
+   
 }
