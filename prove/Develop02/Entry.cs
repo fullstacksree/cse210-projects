ceb7f711088617b9675359483ebcb67190438f6f
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-21 07:18:43 +0530

    Made changes

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,16 +1,24 @@
 public class Entry {
 
     // Prompt is the part of the entry
-    public string _prompt; 
+    //public string _prompt; 
 
     // User answer based on the prompt
-    public string _answer;
+    //public string _answer;
 
     // When the use make an entry with date
 
     public string _date;
 
-    public void Display(){
-        Console.WriteLine($"Date: {_date} - Prompt: { _prompt} { _answer}");
-    }    
+    
+    public string _prompt;
+    Prompt myPrompts = new Prompt();
+    public string Prompt()
+    {
+        _prompt=(string.Format("{0} Prompt:{1}", _date, Prompt()));
+        Console.WriteLine(_prompt);
+        _prompt += " " + Console.ReadLine();
+        return _prompt;
+    }
+    
 }
