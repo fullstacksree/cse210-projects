ca8e8e836ed65761d7771c3f5778bf7f312d6e97
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-21 16:48:57 +0530

    Make changes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference scriptureReference = new Reference("Malachi", "3", "10");
+        Scripture scripture = new Scripture(scriptureReference, "Bring ye all the tithes into the storehouse, that there may be meat in mine house, and prove me now herewith, saith the Lord of hosts, if i will not open you the windows of heaven, and pour you out a blessing, that there shall not be room enough to receive it.");
+        ScriptureMemorizer scriptureMemorizer = new ScriptureMemorizer(scripture);
+
+        string userInput = "";
+
+        while (userInput != "quit" && scriptureMemorizer.hasWordsLeft() == true)
+        {
+            Console.Clear();
+            Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+            Console.WriteLine();
+            Console.ReadLine();
+            scriptureMemorizer.removeWordsFromText();
+        }
     }
 }
