1d40f12ddf26f6d0de2b76b4e5bfcb3c632a8c55
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-21 06:55:32 +0530

    Made Changes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,33 +3,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        bool flag = true;
+        
         Console.WriteLine("Welcome to Daily Journal");
         string choice = "0";
         
         do{
             Console.WriteLine("\n Select one of the below choices:");
             Console.Write("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            Console.Write("What would you like to do? ");
             Journal myJournal = new Journal();
             Prompt myPrompts = new Prompt();
             switch(choice){
                 case "1":
-                    myJournal.Write();
                     break;
                 case "2":
-                    myJournal.Display();
                     break;
                 case "3":
-                    myJournal.Load();
                     break;
                 case "4":
-                    myJournal.Save();
                     break;
                 case "5":
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Enter 5 to Quit or Choose from the list");
                     break;
         }
-        }while (choice != "5");
-        {
-            Console.WriteLine("Goodbye!!");
-        } 
+        }while (flag);
+        
     }
 }
