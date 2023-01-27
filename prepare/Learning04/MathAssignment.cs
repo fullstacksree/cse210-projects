946b69b44bc0871b0bbc2904fc42449157ee0183
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-27 12:17:49 +0530

    Finished Learn04

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -5,7 +5,7 @@ public class MathAssignment : Assignment
 
     // Here we should notice that the syntax in the MathAssignment constructor has 4 parameters
     // and it passes to 2 of them directly to the "basic" constructor, which here known as "Assignment" class constructor
-    public MathAssignment (string studentName, string topic, string textmanualSection, string _issues)
+    public MathAssignment (string studentName, string topic, string textmanualSection, string issues)
         : base(studentName, topic)
     {
         _textmanualSection = textmanualSection;
