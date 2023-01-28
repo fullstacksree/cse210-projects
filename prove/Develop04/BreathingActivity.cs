3a81f95d9b64ee6cef77a867f6e6e1134ffe0ee1
fullstacksree <114227478+fullstacksree@users.noreply.github.com>
2023-01-29 01:22:34 +0530

    Made Changes

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class BreathingActivity
+{
+    Activity _activity = new Activity();
+
+    private int _timeduration;
+
+    public BreathingActivity()
+    {
+        Console.WriteLine("Welcome to Breathing Activity.");
+        Console.WriteLine("This breathing activity will guide you to release from stress by doing the breathing in and out exercise.");
+        Console.WriteLine("How long you would like your session would be /in seconds/");
+        _timeduration = int.Parse(Console.ReadLine());
+        Console.Clear();
+
+        Console.WriteLine("Get Start...");
+        _activity.displaySpinner();
+
+        Countdown();
+
+        Console.WriteLine("Nicely done!");
+        _activity.displaySpinner();
+
+        Console.WriteLine($"You have done another {_timeduration} seconds of breathing activity");
+        _activity.displaySpinner();
+        Console.Clear();
+
+    }
+
+    private void Countdown()
+    {
+        int i = 20;
+        for (int a = _timeduration / i - 1; a >=0; a--)
+        {
+            for (i = 5; i >= 0; i--)
+            {
+                Console.Write("Breath in...{0}", i);
+                Thread.Sleep(1000);
+            }
+            for (i = 5; i >= 0; i--)
+            {
+                Console.Write("Breath out...{0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class ListingActivity
+{
+    private List<string> listingQueries = new List<string>
+    {
+        "What is the feeling you had? ",
+        "How did you feel at that moment? ",
+        "How did you apply that guidance in different aspects of life",
+    };
+
+}
